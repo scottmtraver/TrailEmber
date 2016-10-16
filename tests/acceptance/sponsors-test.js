77ec95865af98eb16e183affe78782432cd4a1f1
Scott <scottmtraver@gmail.com>
2016-10-16 10:44:40 -0600

    Complete sponsor list view acceptance test for navigation

--- tests/acceptance/sponsors-test.js
+++ tests/acceptance/sponsors-test.js
@@ -10,3 +10,35 @@ test('visiting /sponsors', function(assert) {
     assert.equal(currentURL(), '/sponsors');
   });
 });
+
+test('visiting /sponsors clicking edit row link', function(assert) {
+  server.createList('sponsors', 2);
+
+  visit('/sponsors');
+
+  andThen(function() {
+    let button = findWithAssert('.rt-sponsor-edit a').first();
+
+    click(button);
+
+    andThen(function () {
+      assert.equal(currentURL(), '/sponsors/1/edit', 'clicking on first sponsor link navigates to edit that sponsor');
+    });
+  });
+});
+
+test('visiting /sponsors clicking new link', function(assert) {
+  visit('/sponsors');
+
+  andThen(function() {
+    let button = findWithAssert('.rt-sponsor-new').first();
+
+    click(button);
+
+    andThen(function () {
+      assert.equal(currentURL(), '/sponsors/new', 'clicking on create sponsor link takes you to new sponsor route');
+    });
+  });
+});
+
+
