3e0dcae5f4fa51a03dd1425c81b0747ec0e8ffea
Scott <scottmtraver@gmail.com>
2016-10-18 19:30:36 -0600

    Finished validation for sponsors form component

--- app/components/sponsor/sponsor-form.js
+++ app/components/sponsor/sponsor-form.js
@@ -1,9 +1,38 @@
 import Ember from 'ember';
+import { validator, buildValidations } from 'ember-cp-validations';
 
-export default Ember.Component.extend({
+const Validations = buildValidations({
+  'model.name': [
+    validator('presence', true),
+    validator('length', {
+      min: 1,
+      max: 50
+    })
+  ],
+  'model.linkUrl': [
+    validator('presence', true),
+    validator('format', {
+      type: 'url'
+    })
+  ],
+  'model.imageUrl': [
+    validator('presence', true),
+    validator('format', {
+      type: 'url'
+    })
+  ]
+});
+
+export default Ember.Component.extend(Validations, {
+  model: {
+    hasValidated: false
+  },
   actions: {
     save() {
-      this.sendAction('save', this.get('model'));
+      this.set('hasValidated', true);
+      if(this.get('validations.isValid')) {
+        this.sendAction('save', this.get('model'));
+      }
     },
     cancel() {
       this.sendAction('cancel');
