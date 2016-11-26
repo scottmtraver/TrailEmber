cc04db8da48824b5a8ca623d7f56283dc34f221d
Scott <scottmtraver@gmail.com>
2016-11-26 11:59:55 -0700

    Style sponsors form

--- app/components/sponsor/sponsor-form.js
+++ app/components/sponsor/sponsor-form.js
@@ -2,24 +2,33 @@ import Ember from 'ember';
 import { validator, buildValidations } from 'ember-cp-validations';
 
 const Validations = buildValidations({
-  'model.name': [
-    validator('presence', true),
-    validator('length', {
-      min: 1
-    })
-  ],
-  'model.linkUrl': [
+  'model.name': {
+    description: 'Sponsor',
+    validators:[
+      validator('presence', true),
+      validator('length', {
+        min: 1
+      })
+    ]
+  },
+  'model.linkUrl': {
+    description: 'Link',
+    validators: [
     validator('presence', true),
     validator('format', {
       type: 'url'
     })
-  ],
-  'model.imageUrl': [
-    validator('presence', true),
-    validator('format', {
-      type: 'url'
-    })
-  ]
+    ]
+  },
+  'model.imageUrl': {
+    description: 'Image',
+    validators: [
+      validator('presence', true),
+      validator('format', {
+        type: 'url'
+      })
+    ]
+  }
 });
 
 export default Ember.Component.extend(Validations, {
