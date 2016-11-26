37d420b5092ea5f2bfbd8a8cb2ac7d74a943cdd4
Scott <scottmtraver@gmail.com>
2016-11-26 12:13:59 -0700

    Style venue form

--- app/components/venue/venue-form.js
+++ app/components/venue/venue-form.js
@@ -2,30 +2,42 @@ import Ember from 'ember';
 import { validator, buildValidations } from 'ember-cp-validations';
 
 const Validations = buildValidations({
-  'model.name': [
-    validator('presence', true),
-    validator('length', {
-      min: 1
-    })
-  ],
-  'model.linkUrl': [
-    validator('presence', true),
-    validator('format', {
-      type: 'url'
-    })
-  ],
-  'model.directionsUrl': [
-    validator('presence', true),
-    validator('format', {
-      type: 'url'
-    })
-  ],
-  'model.imageUrl': [
-    validator('presence', true),
-    validator('format', {
-      type: 'url'
-    })
-  ]
+  'model.name': {
+    description: 'Name',
+    validators:[
+      validator('presence', true),
+      validator('length', {
+        min: 1
+      })
+    ]
+  },
+  'model.linkUrl':{
+    description: 'Website',
+    validators: [
+      validator('presence', true),
+      validator('format', {
+        type: 'url'
+      })
+    ]
+  },
+  'model.directionsUrl': {
+    description: 'Direcitons',
+    validators: [
+      validator('presence', true),
+      validator('format', {
+        type: 'url'
+      })
+    ]
+  },
+  'model.imageUrl': {
+    description: 'Image',
+    validators: [
+      validator('presence', true),
+      validator('format', {
+        type: 'url'
+      })
+    ]
+  }
 });
 
 export default Ember.Component.extend(Validations, {
