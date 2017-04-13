953c1dc705eba068ce249ee2be3738b6c3eccb22
Scott <scottmtraver@gmail.com>
2017-04-13 00:41:49 -0600

    Styling changes

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,5 +1,4 @@
 import DS from 'ember-data';
-import Ember from 'ember';
 import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
 export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
