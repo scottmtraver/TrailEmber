f627ab8d3c30200800670e4e410d894bb51387c7
Kevin Traver <kevintraver@gmail.com>
2017-04-12 23:42:55 -0600

    Setup ember-simple-auth

--- app/services/auth-manager.js
+++ app/services/auth-manager.js
@@ -2,20 +2,5 @@ import Ember from 'ember';
 
 export default Ember.Service.extend({
   accessToken: null,
-
-  authenticate(login, password) {
-    return Ember.$.ajax({
-      method: "POST",
-      url: "/token",
-      data: { username: login, password: password }
-    }).then((result) => {
-      this.set('accessToken', result.access_token);
-    });
-  },
-
-  invalidate() {
-    this.set('accessToken', null);
-  },
-
   isAuthenticated: Ember.computed.bool('accessToken')
 });
