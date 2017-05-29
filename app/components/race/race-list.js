1224cd8da03f402fcbb175bf140b695fa5933473
Scott <scottmtraver@gmail.com>
2017-05-29 15:59:09 -0600

    Sort race list by date descending

--- app/components/race/race-list.js
+++ app/components/race/race-list.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+    sortedRaces: Ember.computed('model', function() {
+        return this.get('model').sort(function (a, b) {
+            return moment.utc(b.get('date')).diff(moment.utc(a.get('date')))
+        })
+    })
 });
--- app/routes/races/index.js
+++ app/routes/races/index.js
@@ -2,7 +2,8 @@ import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
-  model() {
-    return this.get('store').findAll('race');
+  model: function () {
+    return this.store.findAll('race', { reload: true }).
+      then(orders => orders.sortBy('date'));
   }
 });
