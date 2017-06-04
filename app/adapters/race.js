a10a4d96d38e9bc090c3d55a3061d99c3df2886d
Scott <scottmtraver@gmail.com>
2017-06-03 18:36:13 -0600

    Fix api url for results uploads

--- app/adapters/race.js
+++ app/adapters/race.js
@@ -2,7 +2,7 @@ import ApplicationAdapter from './application';
 
 export default ApplicationAdapter.extend({
   uploadResults(model, params) {
-    const url = this.buildURL('/results');
+    const url = this.buildURL('results');
     return this.ajax(url, 'POST', { data: params.file });
   }
 });
